d8edfbae8f01f9b4d55e059a182f89e9a94cc178
selamair <selamair@gmail.com>
2025-03-10 08:59:02 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("This is going to be fun!");
+console.log("How about we get started!");
+console.log((5 * 5) / 2);
+
+let dayOfWeek = "Thursday";
+//this is how you add a comment
+let todayDate = "6";
+let month = "March";
+let year = "2025;";
+console.log(dayOfWeek, todayDate, month, year);
+dayOfWeek = "friday";
+todayDate = "7";
+console.log(dayOfWeek, todayDate, month, year);
+//store birthday
+const birthDay = "March 6,1981";
+console.log("Birthday", birthDay);
+let unitOfTemp = "Farenheit";
+let tempInF = 53;
+let stringTempInF = "53";
+console.log(tempInF, typeof tempInF);
+console.log(stringTempInF, typeof stringTempInF);
+//booleans
+let isCold = false;
+console.log(isCold, typeof isCold);
+//null vs undefined
+let actualTempTomorrow = null;
+console.log(actualTempTomorrow, typeof actualTempTomorrow);
+//operators
+console.log(10 % 2);
+
+let currentCount = 5;
+let maxNumber = 3;
+let groupNumber = currentCount % maxNumber;
+console.log(groupNumber);
